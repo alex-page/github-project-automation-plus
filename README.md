89716e3fe710672d1a970591139ecaa62497ff78
Alex Page <alex@alexpage.com.au>
2019-08-31 16:48:55 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,63 @@
+# GitHub Project Automation+
+
+> 🤖 Automate GitHub Project cards with any webhook event
+
+This action allows you to use any of the `pull_request` and `issue` [webhook events](https://help.github.com/en/articles/events-that-trigger-workflows#webhook-events) to automate your project cards. For example when an `issue` is `opened` create a card in the Backlog project, Triage column.
+
+If the `pull_request` or `issue` has a card it will be moved to the column provided. Otherwise the card will be created in the column.
+
+
+## Usage
+
+1. Create a new workflow by adding `.github/workflows/backlog-automation.yml` to your project. 
+2. Create a [project](https://help.github.com/en/articles/about-project-boards) with Columns set up in your repository or organisation.
+3. In the `backlog-automation.yml` you have to decide what events and actions are going move an issue or pull request to a column.
+
+
+_For example:_
+
+To move opened issues into the Triage column and assigned pull requests into the To Do column of the Backlog project. You would add the following to the `backlog-automation.yml` file:
+
+```yml
+name: Automate project columns
+
+on: [issues, pull_request]
+
+jobs:
+  automate-project-columns:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Move new issues into Triage
+        if: github.event_name == 'issues' && github.event.action == 'opened'
+        uses: alex-page/automate-project-columns@master
+        with:
+          project: Backlog
+          column: Triage
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+
+      - name: Move assinged pull requests into To do
+        if: github.event_name == 'pull_request' && github.event.action == 'assigned'
+        uses: alex-page/automate-project-columns@master
+        with:
+          project: Backlog
+          column: To do
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+
+## Workflow options
+
+These are the options recommended to be changed. For more detailed explanation of the workflow file, check out the [GitHub documentation](https://help.github.com/en/articles/configuring-a-workflow#creating-a-workflow-file).
+
+| Setting | Description | Values |
+| --- | --- | --- |
+| `on` | When the automation is ran | `[issues, pull_request]` |
+| `github.event.name` | The event type | `issues` or `pull_request` |
+| `github.event.action` | The [webhook event](https://help.github.com/en/articles/events-that-trigger-workflows#webhook-events) that triggers the automation | `opened`, `assigned`, [...more](https://help.github.com/en/articles/events-that-trigger-workflows#webhook-events) |
+| `project` | The name of the project | `Backlog` |
+| `column` | The column to create or move the card to | `Triage` |
+
+
+## Release History
+
+- v0.0.0 - Initial release
---
+++ index.js
@@ -0,0 +1,119 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const token = core.getInput('repo-token');
+const project = core.getInput('project');
+const column = core.getInput('column');
+
+const octokit = new github.GitHub(token);
+
+const getData = () => {
+	const {eventName, payload} = github.context;
+	if (eventName !== 'pull_request' && eventName !== 'issues') {
+		throw new Error(`Only pull requests or issues allowed, received:\n${eventName}`);
+	}
+
+	const githubData = eventName === 'issues' ?
+		payload.issue :
+		payload.pull_request;
+
+	return {
+		eventName,
+		action: payload.action,
+		nodeId: githubData.node_id,
+		url: githubData.html_url
+	};
+};
+
+(async () => {
+	try {
+		const {eventName, action, nodeId, url} = getData();
+
+		// Get the column ID  from searching for the project and card Id if it exists
+		const fetchColumnQuery = `query {
+			resource( url: "${url}" ) {
+				... on ${eventName === 'issues' ? 'Issue' : 'PullRequest'} {
+					projectCards {
+						nodes {
+							id
+						}
+					}
+					repository {
+						projects( search: "${project}", first: 10, states: [OPEN] ) {
+							nodes {
+								id
+								columns( first: 100 ) {
+									nodes {
+										id
+										name
+									}
+								}
+							}
+						}
+						owner {
+							... on Organization {
+								projects( search: "${project}", first: 10, states: [OPEN] ) {
+									nodes {
+										id
+										columns( first: 100 ) {
+											nodes {
+												id
+												name
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}`;
+
+		const {resource} = await octokit.graphql(fetchColumnQuery);
+
+		// All the matching projects found
+		const repoProjects = resource.repository.projects.nodes || [];
+		const orgProjects = (resource.repository.owner &&
+			resource.repository.owner.projects &&
+			resource.repository.owner.projects.nodes) ||
+			[];
+
+		// Search the projects for columns with a name that matches
+		const columns = [...repoProjects, ...orgProjects]
+			.flatMap(projects => {
+				return projects.columns.nodes ?
+					projects.columns.nodes.filter(projectColumn => projectColumn.name === column) :
+					[];
+			});
+
+		const cardId = (resource.projectCards.nodes &&
+			resource.projectCards.nodes[0] &&
+			resource.projectCards.nodes[0].id) ||
+			null;
+
+		if (columns.length === 0) {
+			throw new Error(`Could not find ${column} in ${project}`);
+		}
+
+		// If a card already exists, move it to the column
+		if (cardId) {
+			await Promise.all(
+				columns.map(column => octokit.graphql(`mutation {
+					moveProjectCard( input: { cardId: "${cardId}", columnId: "${column.id}"
+				}) { clientMutationId } }`))
+			);
+		// If the card does not exist, add it to the column
+		} else {
+			await Promise.all(
+				columns.map(column => octokit.graphql(`mutation {
+					addProjectCard( input: { contentId: "${nodeId}", projectColumnId: "${column.id}"
+				}) { clientMutationId } }`))
+			);
+		}
+
+		console.log(`✅ ${action === 'opened' ? 'Added' : 'Moved'} card to ${column} in ${project}`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
