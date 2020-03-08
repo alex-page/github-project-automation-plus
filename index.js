6005e89b5ae47dc0a2013b5bd2e554a16bef01af
Alex Page <alex@alexpage.com.au>
2020-03-07 21:49:10 -0500

    Add key for setOutput

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ const getData = () => {
 			resource.repository.owner.projects.nodes) ||
 			[];
 
-		core.setOutput([...repoProjects, ...orgProjects].toString());
+		core.setOutput('projects', [...repoProjects, ...orgProjects].toString());
 
 		// Get the column data of projects and columns that match input
 		const columns = [...repoProjects, ...orgProjects]
@@ -86,7 +86,7 @@ const getData = () => {
 				[]
 			);
 
-		core.setOutput(columns.toString());
+		core.setOutput('columns', columns.toString());
 
 		if (columns.length === 0) {
 			throw new Error(`Could not find the column "${column}" in project "${project}"`);
