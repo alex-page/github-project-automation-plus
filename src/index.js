46785e7c1a7bd95535c85f90a2ef1276aed03d8e
Alex Page <alex@alexpage.com.au>
2020-03-25 17:56:50 -0400

    Log resource instead of query

--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ const generateMutationQuery = require('./generate-mutation-query');
 		// Get the column ID from searching for the project and card Id if it exists
 		const projectQuery = generateProjectQuery(url, eventName, project);
 
-		console.log(projectQuery);
-
 		const {resource} = await octokit.graphql(projectQuery);
+
+		console.log(resource);
 
 		// A list of columns that line up with the user entered project and column
 		const mutationQueries = generateMutationQuery(resource, project, column, nodeId);
