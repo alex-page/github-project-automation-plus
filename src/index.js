67054bab44a722ffe097191b32086b9d829da920
Alex Page <alex@alexpage.com.au>
2020-03-25 17:33:51 -0400

    v0.2.0

--- src/index.js
+++ src/index.js
@@ -20,14 +20,10 @@ const generateMutationQuery = require('./generate-mutation-query');
 		// Get the column ID from searching for the project and card Id if it exists
 		const projectQuery = generateProjectQuery(url, eventName, project);
 
-		console.log(projectQuery);
-
 		const {resource} = await octokit.graphql(projectQuery);
 
 		// A list of columns that line up with the user entered project and column
 		const mutationQueries = generateMutationQuery(resource, project, column, nodeId);
-
-		console.log(mutationQueries);
 
 		// Run the graphql queries
 		await Promise.all(mutationQueries.map(query => octokit.graphql(query)));
