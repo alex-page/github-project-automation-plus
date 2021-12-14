11e410c8425788aae48ba4ae8a8da6dcebab9012
Thomas Lin <t.lin@mail.utoronto.ca>
2021-12-14 18:35:05 -0500

    Move card for all projects issue is in if 'project' isn't specified in workflow YAML

--- src/index.js
+++ src/index.js
@@ -27,11 +27,28 @@ const generateMutationQuery = require('./generate-mutation-query');
 
 		core.debug(JSON.stringify(resource));
 
-		// A list of columns that line up with the user entered project and column
-		const mutationQueries = generateMutationQuery(resource, project, column, nodeId, action);
-		if ((action === 'delete' || action === 'archive' || action === 'add') && mutationQueries.length === 0) {
-			console.log('✅ There is nothing to do with card');
-			return;
+		// Error if no project specified and issue belongs to no projects
+		if (!project && resource.projectCards.nodes.length === 0) {
+			throw new Error('The issue belongs to no projects and no project has been specified');
+		}
+
+		// If no project was specified, attempt action on all projects the issue is part of
+		const projectList = project ? [project] : resource.projectCards.nodes.map(card => card.project.name);
+
+		var mutationQueries = []
+		var validProjectList = []
+		for (proj of projectList) {
+			try {
+				// A list of columns that line up with the user entered project and column
+				const queries = generateMutationQuery(resource, proj, column, nodeId, action);
+				if ((action === 'delete' || action === 'archive' || action === 'add') && queries.length === 0) {
+					console.log(`✅ There is nothing to do with card in project ${proj}`);
+				}
+				mutationQueries.push(...queries);
+				validProjectList.push(proj);
+			} catch (error) {
+				console.log(error.message);
+			}
 		}
 
 		core.debug(mutationQueries.join('\n'));
@@ -39,10 +56,8 @@ const generateMutationQuery = require('./generate-mutation-query');
 		// Run the graphql queries
 		await Promise.all(mutationQueries.map(query => octokit.graphql(query)));
 
-		if (mutationQueries.length > 1) {
-			console.log(`✅ Card materialised into to ${column} in ${mutationQueries.length} projects called ${project}`);
-		} else {
-			console.log(`✅ Card materialised into ${column} in ${project}`);
+		if (mutationQueries.length) {
+			console.log(`✅ Card materialised into ${column} in ${mutationQueries.length} project(s): ${validProjectList.join(', ')}`);
 		}
 	} catch (error) {
 		core.setFailed(error.message);
